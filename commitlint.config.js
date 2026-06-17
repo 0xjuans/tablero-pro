a89676de4cbb7a915797de2ea021ef246c7ace61
Juan Quinto Personal <juans.quintoh@hotmail.com>
2026-06-17 15:23:41 -0500

    chore: initial project setup with best practices

---
+++ commitlint.config.js
@@ -0,0 +1,15 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'perf', 'ci', 'revert'],
+    ],
+    'subject-case': [2, 'never', ['upper-case']],
+    'subject-max-length': [2, 'always', 100],
+  },
+};
+
+module.exports = config;
